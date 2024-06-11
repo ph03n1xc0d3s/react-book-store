83480fb341411c4a097632237352c4f8a5ba53b8
ph03n1xc0d3s <ph03n1xc0d3s@gmail.com>
2024-06-11 17:28:32 +0530

    added onClick event on subscribe button

--- src/components/NewsLetter.js
+++ src/components/NewsLetter.js
@@ -1,17 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 const NewsLetter = () => {
+  const [subscribe, setSubscribe] = useState(false);
+
+  const handleNewsLetterSubscription = () => {
+    setSubscribe(true);
+  };
+
   return (
-      <div className="card bg-dark">
+    <div className={`card ${subscribe ? "bg-light" : "bg-dark"}`}>
       <div className="card-title text-center mt-3 mb-1">
-        <h4 className="text-light">Subscribe for new updates!</h4>
+        <h4 className={`text-light ${subscribe ? 'd-none' : 'd-block'}`}>Subscribe for new updates!</h4>
       </div>
-        <div className="card-body d-flex gap-2">
-            <label htmlFor=""></label>
-            <input type="text" className="form-control" placeholder="Enter Email Address"/>
-            <button className="btn btn-sm btn-primary">Subscribe</button>
-        </div>
+      <div className="card-body d-flex gap-2 justify-content-center">
+        {subscribe != true ? (
+          <>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Enter Email Address"
+              id="email"
+              required
+            />
+            <button
+              className="btn btn-sm btn-primary"
+              onClick={handleNewsLetterSubscription}
+            >
+              Subscribe
+            </button>
+          </>
+        ) : <p className="text-center text-success">Subscribed Successfully</p>}
       </div>
+    </div>
   );
 };
 
